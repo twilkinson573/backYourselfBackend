b9de0581f626ee6179265eccffcdb22506c81a80
tuds <twilkinson573@gmail.com>
2022-04-06 16:16:46 +0100

    Add initial data structs for creating & tracking Wagers

--- contracts/WagerManager.sol
+++ contracts/WagerManager.sol
@@ -3,41 +3,66 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract WagerManager is Ownable {
+  using Counters for Counters.Counter;
+  Counters.Counter private _wagerIds;
 
   struct Wager {
     string description;
     address address0;
     address address1;
-    uint8 address0Verdict; // null == no verdict given yet, 0 == 'I lost' verdict, 1 == 'I won' verdict
+    uint8 status;          // 0 == 'proposed', 1 == 'denied', 2 == 'active', 3 == 'complete'
+    uint8 address0Verdict; // 0 == no verdict given yet, 1 == 'I lost' verdict, 2 == 'I won' verdict
     uint8 address1Verdict;
-    uint depositSize;
-    uint depositDate;
+    uint wagerSize;
   }
+
+  // Mapping from user to list of the wager id's they created
+  mapping(address => uint[]) private _userWagers;
+
+  // Mapping from wager id to Wager object
+  mapping(uint => Wager) private _allWagers;
 
   // Mapping of addresses to human-friendly nicknames
   mapping(address => string) private _playerNicknames;
 
+  constructor() {}
 
-  // // Mapping from owner to list of owned token IDs
-  // mapping(address => mapping(uint256 => uint256)) private _ownedTokens;
-  // // Mapping from token ID to index of the owner tokens list
-  // mapping(uint256 => uint256) private _ownedTokensIndex;
-  // // Array with all token ids, used for enumeration
-  // uint256[] private _allTokens;
-  // // Mapping from token id to position in the allTokens array
-  // mapping(uint256 => uint256) private _allTokensIndex;
-  // // Mapping from token id to Ticket MetaData
-  // // TODO2 - remove public modifier (just for dev -> inspection)
-  // mapping(uint => LotteryTicketMeta) public lotteryTickets;
+  // WAGERS ===================================================================
+  
+  function createWager(
+    address _opponent, 
+    uint _wagerSize, 
+    string calldata _description
+  ) external payable {
+    // take money
 
+    // create wager
+    Wager memory _newWager = Wager(
+      _description,
+      msg.sender,
+      _opponent,
+      0,
+      0,
+      0,
+      _wagerSize
+    );
 
-  constructor() {
+    // add to _userWagers for both users & _allWagers
+    _userWagers[msg.sender].push(_wagerIds.current());
+    _userWagers[_opponent].push(_wagerIds.current());
+
+    _allWagers[_wagerIds.current()] = _newWager;
+
+    _wagerIds.increment();
   }
 
-  function setNickname(string calldata _nickname) public {
+
+  // NICKNAMES ================================================================
+
+  function setNickname(string calldata _nickname) external {
     _playerNicknames[msg.sender] = _nickname;
   }
 
