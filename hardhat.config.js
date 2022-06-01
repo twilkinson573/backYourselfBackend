34324a6e769768c891bc7f55b5be96aee056cf65
tuds <twilkinson573@gmail.com>
2022-06-01 13:04:44 +0100

    Add deploy script, add etherscan package for verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 require('dotenv').config();
 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,14 +2,14 @@ const hre = require("hardhat");
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
-  
+  console.log("Deployer address: ", deployer.address);
+
   const Wm = await hre.ethers.getContractFactory("WagerManager");
-  const wm = await Wm.deploy(usdc.address);
+  const wm = await Wm.deploy(process.env.USDC_ADDRESS);
   await wm.deployed();
+
   console.log("Wager manager deployed to: ", wm.address);
 
-  await usdc.connect(bob).approve(wm.address, ethers.utils.parseUnits("1000000"));
-  await usdc.connect(jane).approve(wm.address, ethers.utils.parseUnits("1000000"));
 }
 
 main()
